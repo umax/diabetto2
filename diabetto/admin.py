d00364c200e1930d91f0c21b8f7cd1f2ab9960a8
Max Usachev <maxusachev@gmail.com>
2014-01-24 19:18:57 +0300

    Added admin interface.

---
+++ diabetto/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
