47856266342dcfa25b5c19b20adddba539052626
Max Usachev <maxusachev@gmail.com>
2014-06-23 18:28:18 +0300

    Parse product weights from POST data.

--- dish/forms.py
+++ dish/forms.py
@@ -19,3 +19,35 @@ class DishForm(ModelForm):
         self.fields['name'].error_messages.update({
             'required': u'Название блюда не может быть пустым',
         })
+
+    def _clean_weights(self):
+        """
+        Extract weights for products from POST data.
+        """
+
+        postfix = 'weight'
+        weights = {}
+        for key, value in self.data.iteritems():
+            if key.endswith(postfix):
+                product_id, _ = key.split('_')
+                try:
+                    weights[product_id] = int(value)
+                except ValueError:
+                    continue
+
+        return weights
+
+    def clean(self):
+        cleaned_data = super(DishForm, self).clean()
+        cleaned_data['weights'] = self._clean_weights()
+
+        return cleaned_data
+
+    def save_products(self, dish):
+        pass
+
+    def save(self, *args, **kwargs):
+        dish = super(DishForm, self).save(*args, **kwargs)
+        self.save_products(dish)
+
+        return dish
